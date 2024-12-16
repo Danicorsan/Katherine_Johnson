4e18f1ea0086a54844500597989b8c60f53066b6
Sergio Aguilera Ibarra <al.sergio.aguilera.ibarra@iesportada.org>
2024-12-16 16:26:28 +0100

    refactor: pantalla detalles de producto

--- features/productdetail/src/main/java/app/features/productdetail/ui/components.kt
+++ features/productdetail/src/main/java/app/features/productdetail/ui/components.kt
@@ -1,19 +1,142 @@
 package app.features.productdetail.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import app.base.ui.composables.HighSpace
+import app.base.ui.composables.MediumSpace
+import app.base.ui.composables.Separations
 import app.base.ui.composables.SmallSpace
+import app.base.utils.format
+import app.base.utils.toCurrency
+import app.domain.invoicing.product.Product
 import app.domain.invoicing.product.ProductState
+import app.domain.invoicing.product.complements.notes.Note
 import app.features.productdetail.R
 
 @Composable
-fun ProductDetailsColumn(modifier: Modifier = Modifier, dataType : String, data : String?){
+fun ScrollableContentColumn(product: Product){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(top = Separations.Medium),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ){
+        TitleText(product.name)
+        MediumSpace()
+        MakeInRow(
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.code_label), data = product.code
+            )},
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.short_name_label), data = product.shortName
+            )}
+        )
+        MediumSpace()
+        MakeInRow(
+            { ProductDetailsColumn(
+                dataType = stringResource(R.string.category_label), data = product.category.name
+            )},
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.serial_number_label), data = product.serialNumber
+            )}
+        )
+
+        MediumSpace()
+        MakeInRow(
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.model_code_label), data = product.modelCode
+            )},
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.product_type_label), data = product.productType
+            )}
+        )
+        MediumSpace()
+
+        MakeInRow(
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.section_label), data = product.section
+            )},
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.state_label), state = product.state
+            )}
+        )
+        MediumSpace()
+        MakeInRow(
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.stock_label), data = product.stock.toString()
+            )},
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.price_label), data = product.price.toCurrency()
+            )}
+        )
+        MediumSpace()
+        MakeInRow(
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.acquisition_date_label), data = product.acquisitionDate.format()
+            )},
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.discontinuation_date_label), data = product.discontinuationDate?.format()
+            )}
+        )
+        MediumSpace()
+        MakeInRow(
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.minimun_stock_label), data = product.minimunStock?.toString()
+            )},
+            {ProductDetailsColumn(
+                dataType = stringResource(R.string.labels_label), data = if (product.tags.isEmpty()) null else product.tags.toString()
+            )}
+        )
+        MediumSpace()
+        ProductDetailsColumn(
+            dataType = stringResource(R.string.description_label)
+        ){
+            val contenidoAMostrar = when(product.description){
+                null -> stringResource(R.string.field_without_value)
+                else -> product.description!!
+            }
+            DescriptionText(contenido = contenidoAMostrar)
+        }
+        MediumSpace()
+        ProductDetailsColumn(
+            dataType = stringResource(R.string.notes_label)
+        ){
+            ScrollabelRow(product.notes) {
+                    nota -> ShowNote(nota)
+            }
+        }
+    }
+}
+
+@Composable
+private fun ShowNote(note: Note){
+    Column {
+        InformativeText(text = note.title)
+        SmallSpace()
+        InformativeText(text = note.title)
+    }
+}
+
+
+@Composable
+fun ProductDetailsColumn(dataType : String, data : String?){
     Column (
-        modifier = modifier,
+        modifier = Modifier.width(175.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         TextTag(text = dataType)
@@ -25,7 +148,7 @@ fun ProductDetailsColumn(modifier: Modifier = Modifier, dataType : String, data 
 }
 
 @Composable
-fun ProductDetailsColumn(modifier: Modifier = Modifier, dataType : String, state : ProductState){
+fun ProductDetailsColumn(dataType : String, state : ProductState){
     val textoMostrar = when(state){
         ProductState.new -> stringResource(R.string.state_as_new)
         ProductState.modified -> stringResource(R.string.state_as_modified)
@@ -52,10 +175,12 @@ fun MakeInRow(
     productDetail1 : @Composable () -> Unit,
     productDetail2: @Composable () -> Unit
 ){
-    Row(
-    ) {
-        productDetail1()
-        LargeSpace()
-        productDetail2()
-    }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            productDetail1()
+            HighSpace()
+            productDetail2()
+        }
 }
--- features/productdetail/src/main/java/app/features/productdetail/ui/productdetailview.kt
+++ features/productdetail/src/main/java/app/features/productdetail/ui/productdetailview.kt
@@ -1,10 +1,12 @@
 package app.features.productdetail.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,13 +27,15 @@ import java.util.Date
 
 @Preview(showBackground = true)
 @Composable
-fun ProductDetailScreen(){
+fun ProductDetailScreen(
+    onGoBackNav: () -> Unit = {}
+){
     val category = Category (
         id = 3,
         name = "Nombre Categoria",
         shortName = "cat",
         description = "",
-        image = byteArrayOf(12,23,14).toString(),
+        image = "Una imagen",
         createdAt = Date(342422424),
         fungible = false
     )
@@ -52,121 +56,29 @@ fun ProductDetailScreen(){
         acquisitionDate = Instant.parse("2023-06-10T15:00:00Z"),
     )
     product.notes.add(Note("Un titulo genial", "Un cuerpo muy pobreeeeee"))
-    ProductDetailContent(Modifier, product)
+    ProductDetailContent(
+        product = product,
+        onGoBackNav = onGoBackNav
+        )
 }
 
 @Composable
-private fun ProductDetailContent(modifier: Modifier, product : Product){
-
-    Column {
-        Appbar(titleText = stringResource(R.string.title_appbar))
-        ScrollableContentColumn(modifier = modifier ,product = product)
-    }
-}
-
-@Composable
-fun ScrollableContentColumn(modifier: Modifier = Modifier, product: Product){
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(top = Separations.Medium),
-        horizontalAlignment = Alignment.CenterHorizontally,
-
-        ){
-
-        TitleText(modifier.padding(Separations.Medium), product.name)
-
-        MakeInRow(
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.code_label), data = product.code
-            )},
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.short_name_label), data = product.shortName
-            )}
-        )
-        MediumSpace()
-        MakeInRow(
-            { ProductDetailsColumn(
-                dataType = stringResource(R.string.category_label), data = product.category.name
-            )},
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.serial_number_label), data = product.serialNumber
-            )}
-        )
-
-        MediumSpace()
-        MakeInRow(
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.model_code_label), data = product.modelCode
-            )},
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.product_type_label), data = product.productType
-            )}
-        )
-        MediumSpace()
-
-        MakeInRow(
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.section_label), data = product.section
-            )},
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.state_label), state = product.state
-            )}
-        )
-        MediumSpace()
-        MakeInRow(
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.stock_label), data = product.stock.toString()
-            )},
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.price_label), data = product.price.toCurrency()
-            )}
-        )
-        MediumSpace()
-        MakeInRow(
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.acquisition_date_label), data = product.acquisitionDate.format()
-            )},
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.discontinuation_date_label), data = product.discontinuationDate?.format()
-            )}
-        )
-        MediumSpace()
-        MakeInRow(
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.minimun_stock_label), data = product.minimunStock?.toString()
-            )},
-            {ProductDetailsColumn(
-                dataType = stringResource(R.string.labels_label), data = if (product.tags.isEmpty()) null else product.tags.toString()
-            )}
-        )
-        MediumSpace()
-        ProductDetailsColumn(
-            dataType = stringResource(R.string.description_label)
-        ){
-            val contenidoAMostrar = when(product.description){
-                null -> stringResource(R.string.field_without_value)
-                else -> product.description!!
-            }
-            TextoDescripcion(contenido = contenidoAMostrar)
+private fun ProductDetailContent(
+    product : Product,
+    onGoBackNav : () -> Unit
+){
+    Scaffold(
+        topBar = @Composable {
+            Appbar(
+                titleText = stringResource(R.string.title_appbar),
+                navigationAction = onGoBackNav
+            )
         }
-        MediumSpace()
-        ProductDetailsColumn(
-            dataType = stringResource(R.string.notes_label)
-        ){
-            FilaDeContenidoScrolleable(product.notes) {
-                    nota -> ShowNote(nota)
-            }
+    ){ contentPadding ->
+        Box(
+            modifier = Modifier.padding(contentPadding)
+        ) {
+            ScrollableContentColumn(product = product)
         }
-    }
-}
-
-@Composable
-private fun ShowNote(note: Note){
-    Column {
-        InformativeText(text = note.title)
-        SmallSpace()
-        InformativeText(text = note.title)
     }
 }
