e80782766c338724cbcbdfe0b514c17d9dbc9bf3
Sergio Aguilera Ibarra <al.sergio.aguilera.ibarra@iesportada.org>
2025-01-24 08:45:44 +0100

    Cambio visual en creación de productos
    Ahora los campos de texto ocupan en ancho el 90% de
    la pantalla.

--- features/productcreation/src/main/java/app/features/productcreation/ui/base/Specification.kt
+++ features/productcreation/src/main/java/app/features/productcreation/ui/base/Specification.kt
@@ -1,7 +1,7 @@
 package app.features.productcreation.ui.base
 
 object Specification {
-    const val EDITTEXTMAXWIDTHFRACTION : Float = 2/3f
+    const val EDITTEXTMAXWIDTHFRACTION : Float = 90/100f
     const val SIZEOFMAINBOXFRACTION : Float = 90/100f
     const val OBLIGATORYFIELDSMARK : String = "*"
 }
