baef6463ae6cacd95565d5fabd38814969116eef
Sergio Aguilera Ibarra <al.sergio.aguilera.ibarra@iesportada.org>
2025-01-27 18:03:34 +0100

    feature: Añadidos los tres alert dialog faltantes en edición de productos
    Se ha añadido los tres alert dialogs faltantes en la pantalla
    de edición de producto. Estos elementos avisan al usuario cuando:
    1. No se ha podido registrar el producto.
    2. Hay campos obligatorios vacios
    3. Se ha podido actualizar el producto de forma exitosa (volviendo
    a la pantalla anterior en caso despues de que el usuario presione
    "ok")

--- features/productcreation/src/main/java/app/features/productcreation/ui/edition/productoetitionview.kt
+++ features/productcreation/src/main/java/app/features/productcreation/ui/edition/productoetitionview.kt
@@ -1,7 +1,6 @@
 package app.features.productcreation.ui.edition
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,6 +15,7 @@ import app.features.productcreation.R
 import app.features.productcreation.ui.base.components.ProductForm
 import app.features.productcreation.ui.base.ProductViewState
 import app.features.productcreation.ui.base.ProductEvents
+import app.features.productcreation.ui.base.components.AlertDialogOK
 import app.features.productcreation.ui.base.components.ProductCreationAppbar
 import app.features.productcreation.ui.base.components.ProductCreationFloatingActionButton
 
@@ -46,6 +46,21 @@ private fun ProductEditionHost(
     ) { contentPadding ->
         when {
             productState.isLoading -> LoadingUi()
+            productState.errorDataState.cantRegisterProduct -> AlertDialogOK(
+                title = stringResource(R.string.cant_register_alert_dialog_title),
+                message = stringResource(R.string.cant_update_alert_dialog_message),
+                onDismiss = productEvents.onDismissCantRegisterProductAlertDialog
+            )
+            productState.errorDataState.emptyFields -> AlertDialogOK(
+                title = stringResource(R.string.empty_fields_alert_dialog_title),
+                message = stringResource(R.string.empty_fields_alert_dialog_message),
+                onDismiss = productEvents.onDismissEmptyFieldsAlertDialog
+            )
+            productState.productRegisterSuccessful -> AlertDialogOK(
+                title = stringResource(R.string.product_registered_successful_alert_dialog_title),
+                message = stringResource(R.string.product_updated_successful_alert_dialog_message),
+                onDismiss = productEvents.onDismissProductHasBeenRegisteredAlertDialog
+            )
             else -> ProductEditionContent(
                 modifier = Modifier.padding(contentPadding),
                 productState = productState,
