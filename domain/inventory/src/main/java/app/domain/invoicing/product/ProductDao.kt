2985a471e21d3784631c539261cb1fa73c8b832b
Sergio Aguilera Ibarra <al.sergio.aguilera.ibarra@iesportada.org>
2025-03-02 17:51:39 +0100

    Mas

--- domain/inventory/src/main/java/app/domain/invoicing/product/ProductDao.kt
+++ domain/inventory/src/main/java/app/domain/invoicing/product/ProductDao.kt
@@ -6,7 +6,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ProductDao {
@@ -20,7 +19,7 @@ interface ProductDao {
     suspend fun update(product: Product)
 
     @Query("SELECT * FROM Product")
-    suspend fun getAllProducts() : Flow<List<Product>>
+    suspend fun getAllProducts() : List<Product>
 
     @Query("SELECT * FROM Product WHERE id = :productId")
     suspend fun getProductById(productId : Int)
